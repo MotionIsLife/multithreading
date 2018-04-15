7ef0d558035c7ba861bd63aa68f0b9cfe3150a65
kprikhodko <1_v_pole@inbox.ru>
2018-04-15 17:08:31 +0300

    Initial commit

---
+++ src/main/java/General/Demo1.java
@@ -0,0 +1,17 @@
+package General;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            new HelloThread().start();
+        }
+        System.out.println("Hello from main thread");
+    }
+
+    private static class HelloThread extends Thread {
+        @Override
+        public void run() {
+            System.out.println("Hello from " + getName());
+        }
+    }
+}
---
+++ src/main/java/General/Demo2.java
@@ -0,0 +1,17 @@
+package General;
+
+public class Demo2 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            new Thread(new HelloRunnableThread()).start();
+        }
+        System.out.println("Hello from main thread");
+    }
+
+    private static class HelloRunnableThread implements Runnable {
+
+        public void run() {
+            System.out.println("Hello from " + Thread.currentThread().getName());
+        }
+    }
+}
